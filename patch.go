9825de78dc47f9557663af7a48bc01624a224b6b
Alec Scott <alec@bcs.sh>
2021-03-13 10:11:17 -0700

    Versions may carry between lines

--- patch.go
+++ patch.go
@@ -3,6 +3,7 @@ package parspack
 import (
 	"bytes"
 	"fmt"
+	"regexp"
 	"strings"
 	"text/template"
 
@@ -30,10 +31,14 @@ func PatchVersion(input pkg.Package, inputRaw string) (result string, err error)
 
 	versions := buff.String()
 
-	rawData := strings.Split(inputRaw, "version(")
-	beginning := strings.TrimRight(strings.Split(rawData[0], "url")[0], " ")
+	urlExp := regexp.MustCompile(`url\s*=.*\n`)
+	versionExp := regexp.MustCompile(`version\([^\)]*\)`)
+
+	rawData := versionExp.Split(inputRaw, -1)
+	rawUrl := urlExp.Split(rawData[0], 2)
+	beginning := strings.TrimRight(rawUrl[0], " ")
 	end := strings.SplitN(rawData[len(rawData)-1], "\n", 2)
-	result = beginning + fmt.Sprintf("    url      = \"%s\"\n\n", input.URL) + versions + end[1]
+	result = beginning + fmt.Sprintf("    url      = \"%s\"\n", input.URL) + strings.TrimSuffix(rawUrl[1], "    ") + versions + end[1]
 
 	return result, nil
 }
